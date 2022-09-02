47277f71c3a527a97ed49951b0908156d6f12e60
nikki eke <nikkieke001@gmail.com>
2022-09-02 16:47:31 +0100

    First commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,13 @@
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
